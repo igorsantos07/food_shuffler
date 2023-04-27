622256b54c047ba68e35692ef47ca55be5866d52
Igor Santos <igorsantos07@gmail.com>
2023-04-27 01:52:20 -0300

    guesswork(?) on build paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
+	import { base } from '$app/paths'
 	import { page } from '$app/stores'
 </script>
 
 <h1>Food Shuffler {$page.data.hasOwnProperty('subtitle')? ` - ${$page.data.subtitle}` : ''}</h1>
 
 <nav>
-<a href="/">Home</a> |
-<a href="/options">Opções</a> |
-	<a href="/option">Criar nova opção</a>
+<a href={base}>Home</a> |
+<a href="{base}/options">Opções</a> |
+	<a href="{base}/option">Criar nova opção</a>
 </nav>
 <hr/>
 
--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV == 'production'? '/food_shuffler' : ''
+		}
 	}
 };
 
