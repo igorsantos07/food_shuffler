7216fd17b89e227cb80cc8fb71d49ea7e068b20f
Igor Santos <igorsantos07@gmail.com>
2023-04-27 01:40:32 -0300

    what now

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build/food_shuffler',
-			assets: 'build/food_shuffler',
+			// pages: 'build/food_shuffler',
+			// assets: 'build/food_shuffler',
 			fallback: 'index.html'
 		})
 	}
